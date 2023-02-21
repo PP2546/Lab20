4c41a96e6f62f8a0a243b5a957ef4424285fa734
Your Name <you@example.com>
2023-02-21 16:58:02 +0700

    Full

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -1,3 +1,4 @@
+
 #include<iostream>
 using namespace std;
 
@@ -5,8 +6,11 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect R1,Rect R2){
+    double Lx,Ly;
+    
+    Lx = min(R1.x+R1.w,R2.x+R2.w) - max(R2.x,R2.x);
+    Ly = min(R1.y,R2.y) - max(R1.y-R1.h,R2.y-R2.h);
+    if(Lx < 0 or Ly < 0) return 0;
+    else return Lx*Ly;
 }
